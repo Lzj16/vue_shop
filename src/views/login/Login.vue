389d5d3ce1a16caa4610b539a280d515866804f6
luzengjie <911833871@qq.com>
2020-10-26 16:09:05 +0800

    添加初始用户名密码

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -35,7 +35,7 @@ export default {
             // 表单的数据绑定对象
             loginForm:{
                 username:'admin',
-                passowrd:'123456'
+                passowrd:''
             },
             // 表单校验规则
             loginFormRules:{
